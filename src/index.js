da9a7b60f7baec15e9040f83b0411358bda5c766
yajnasteju <yajnasteju@gmail.com>
2019-12-20 15:15:13 +0530

    Testing commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello Sanjay</h1>
+      <h1>Hello Sanjay Teju</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
